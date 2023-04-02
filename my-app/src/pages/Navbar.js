4d1762e0da7954850b84453c7a26d66135fd9f2a
Idil Dirie <idilad07@gmail.com>
2023-04-02 08:57:41 -0400

    navbar component in progress

---
+++ my-app/src/pages/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Hello again </h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+      </ul>
+    </nav>
+  );
+}
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "./Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello World 🚀</h1>
+        <Navbar />
       </main>
     </>
   );
