4397ada923b2dc69fb241c5a922d37b6a2dd4d08
Idil Dirie <idilad07@gmail.com>
2023-04-03 16:23:45 -0400

    small changes

--- my-app/src/pages/Navbar.js
+++ my-app/src/pages/Navbar.js
@@ -1,12 +1,14 @@
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Hello again </h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-      </ul>
-    </nav>
+    <div className="nav-div">
+      <nav>
+        <h1>Navbar </h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+        </ul>
+      </nav>
+    </div>
   );
 }
