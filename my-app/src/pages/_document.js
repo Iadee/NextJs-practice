c8598aa10253fa6e39e32d539e9bb2c2e6dee9cc
Idil Dirie <idilad07@gmail.com>
2023-04-04 10:23:57 -0400

    cleared css added main

--- my-app/src/pages/_document.js
+++ my-app/src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+
         <NextScript />
       </body>
     </Html>
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "./Navbar";
+import Heading from "./Heading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Navbar />
+        <Heading />
       </main>
     </>
   );
