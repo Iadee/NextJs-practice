bc4411d6ac2a9ee3f002a2723b35299b671699b7
Idil Dirie <idilad07@gmail.com>
2023-04-05 10:25:41 -0400

    still in progress

--- my-app/src/pages/Heading.js
+++ my-app/src/pages/Heading.js
@@ -2,7 +2,9 @@ export default function Heading() {
   return (
     <div>
       <h1>This is the main page</h1>
-      <p>blah blah blah</p>
+      <p>Text goes here</p>
+      <p>will be using props soon </p>
+      <p>work in progres......</p>
     </div>
   );
 }
