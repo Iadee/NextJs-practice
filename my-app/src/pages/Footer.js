1814955bcd9a6bfc8ff699aea0c916536202c3e5
Idil Dirie <idilad07@gmail.com>
2023-05-27 16:59:29 -0400

    footer msg added

--- my-app/src/pages/Footer.js
+++ my-app/src/pages/Footer.js
@@ -3,6 +3,7 @@ export default function Footer() {
     <div>
       <footer>this is the footer</footer>
       <footer>will be using props soon </footer>
+      <footer>i will also be using apis </footer>
     </div>
   );
 }
